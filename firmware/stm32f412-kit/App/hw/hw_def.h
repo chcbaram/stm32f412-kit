8d63db4443c39510a6331669df0763659ea49c47
Hancheol Cho <chcbaram@gmail.com>
2024-09-04 23:41:11 +0900

    초기 프로젝트 추가

---
+++ firmware/stm32f412-kit/App/hw/hw_def.h
@@ -0,0 +1,37 @@
+#ifndef HW_DEF_H_
+#define HW_DEF_H_
+
+
+#include "main.h"
+#include "def.h"
+
+
+#define _DEF_FIRMWATRE_VERSION    "V240904R1"
+#define _DEF_BOARD_NAME           "STM32F412-KIT-FW"
+
+
+#define _USE_HW_LED
+#define      HW_LED_MAX_CH          1
+
+#define _USE_HW_UART
+#define      HW_UART_MAX_CH         1
+#define      HW_UART_CH_SWD         _DEF_UART1
+#define      HW_UART_CH_CLI         _DEF_UART1
+
+#define _USE_HW_LOG
+#define      HW_LOG_CH              _DEF_UART1
+#define      HW_LOG_BOOT_BUF_MAX    2048
+#define      HW_LOG_LIST_BUF_MAX    4096
+
+#define _USE_HW_CLI
+#define      HW_CLI_CMD_LIST_MAX    32
+#define      HW_CLI_CMD_NAME_MAX    16
+#define      HW_CLI_LINE_HIS_MAX    8
+#define      HW_CLI_LINE_BUF_MAX    64
+
+
+void logPrintf(const char *fmt, ...);
+void delay(uint32_t time_ms);
+uint32_t millis(void);
+
+#endif
